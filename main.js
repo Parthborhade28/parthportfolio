da06c33f9110e35e5822e4b4ed85d1b69ba3719b
Parth <105780094+Parthborhade28@users.noreply.github.com>
2026-07-09 22:27:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
 
-const typed = new Typed('.multiple-text', {
-    strings:['Frontend Developer', 'Web Designer'],
-    typeSpeed:70,
-    backSpeed:70,
-    backDelay:1000,
-    loop:true,
-});
+// const typed = new Typed('.multiple-text', {
+//     strings:['Frontend Developer', 'Web Designer'],
+//     typeSpeed:70,
+//     backSpeed:70,
+//     backDelay:1000,
+//     loop:true,
+// });
+
+:root{
+    --bg-color:#0f172a;
+    --snd-bg-color:#1e293b;
+    --text-color:#f8fafc;
+    --main-color:#38bdf8;
+}
